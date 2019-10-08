81b5bc77e024ab41d6cc97b61e38c4f7c43eeaa8
theorm <theorm@gmail.com>
2019-10-08 14:14:34 +0200

    - added modifiers

--- test/lib/converters/db.test.js
+++ test/lib/converters/db.test.js
@@ -1,5 +1,6 @@
 /* eslint-env mocha */
 const assert = require('assert')
+const { uniq, zip } = require('lodash')
 const { toNeo4jJson, fromNeo4jJson } = require('../../../lib/converters/db')
 
 describe('toNeo4jJson and fromNeo4jJson', () => {
@@ -49,5 +50,66 @@ describe('toNeo4jJson and fromNeo4jJson', () => {
     assert.deepEqual(neo4jJson, expectedNeo4jObject)
 
     assert.deepEqual(fromNeo4jJson(neo4jJson, options), testJson)
+  })
+
+  it('converts with modifiers', () => {
+    const serializerOptions = {
+      modifiers: {
+        context: (key, contextObject, separator) => Object.keys(contextObject)
+          .reduce((newObj, languageCode) => {
+            const contextPairs = contextObject[languageCode]
+            // eslint-disable-next-line no-param-reassign
+            newObj[`${key}${separator}${languageCode}${separator}starts`] = contextPairs.map(([start]) => start)
+            // eslint-disable-next-line no-param-reassign
+            newObj[`${key}${separator}${languageCode}${separator}ends`] = contextPairs.map(([, end]) => end)
+            return newObj
+          }, {})
+      }
+    }
+
+    const deserializerOptions = {
+      modifiers: {
+        context: (key, contextObject, separator) => {
+          const languageCodes = uniq(Object.keys(contextObject)
+            .map(k => k.split(separator)[1]))
+
+          const context = languageCodes.reduce((newObj, languageCode) => {
+            const starts = contextObject[`${key}${separator}${languageCode}${separator}starts`] || []
+            const ends = contextObject[`${key}${separator}${languageCode}${separator}ends`] || []
+            assert.ok(starts.length === ends.length, `Start and end lists for language ${languageCode} must be of the same size`)
+            // eslint-disable-next-line no-param-reassign
+            newObj[languageCode] = zip(starts, ends)
+            return newObj
+          }, {})
+
+          return { [key]: context }
+        }
+      }
+    }
+
+    const inputObject = {
+      foo: {
+        bar: 'baz'
+      },
+      bang: 1,
+      context: {
+        en: [[3, 10], [16, 23]],
+        fr: [[3, 10]],
+      }
+    }
+    const expectedNeo4jObject = {
+      foo__bar: 'baz',
+      bang: 1,
+      context__en__starts: [3, 16],
+      context__en__ends: [10, 23],
+      context__fr__starts: [3],
+      context__fr__ends: [10],
+    }
+
+    const neo4jJson = toNeo4jJson(inputObject, serializerOptions)
+    assert.deepEqual(neo4jJson, expectedNeo4jObject)
+
+    const reconstructedObject = fromNeo4jJson(neo4jJson, deserializerOptions)
+    assert.deepEqual(reconstructedObject, inputObject)
   })
 })
