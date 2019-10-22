48a7a9d7f76d8b3902b2d556164f63939c470f73
theorm <theorm@gmail.com>
2019-10-22 15:51:31 +0200

    neo4j import pipeline fixes

--- scripts/tools/load-csv-into-db.sh
+++ scripts/tools/load-csv-into-db.sh
@@ -16,13 +16,17 @@ print_help_and_exit() {
 
 [ -d "$CSV_ROOT" ] || print_help_and_exit "CSV directory does not exist"
 [ -d "$DB_ROOT" ] || print_help_and_exit "DB directory does not exist"
-[ -n "$TAG" ] || print_help_and_exit "Tag must be provided"
 [ -n "$DOCKER_EXEC" ] || print_help_and_exit "Docker executable not found"
 
 CSV_ROOT=`realpath $CSV_ROOT`
 DB_ROOT=`realpath $DB_ROOT`
 
 echo "Reading file from '$CSV_ROOT' and writing to database in '$DB_ROOT'"
+
+RESOURCE_FILES=$(find $CSV_ROOT/resource -type f | xargs -L1 -I{} basename "{}" | xargs -L1 -I{} echo "/csvs/resource/{},")
+# RESOURCE_FILES=$(find $CSV_ROOT/resource -type f -exec echo '/csvs/resource/{},' \;)
+# APPEARS_IN_FILES=$(find $CSV_ROOT/appears_in -type f -printf '/csvs/appears_in/%f,')
+APPEARS_IN_FILES=$(find $CSV_ROOT/appears_in -type f | xargs -L1 -I{} basename "{}" | xargs -L1 -I{} echo "/csvs/appears_in/{},")
 
 $DOCKER_EXEC \
   run -it --rm \
@@ -31,10 +35,8 @@ $DOCKER_EXEC \
   neo4j \
   neo4j-admin import \
   --nodes "/csvs/headers/entity.csv,/csvs/entity.csv" \
-  --nodes "/csvs/headers/resource.csv,/csvs/resource/$TAG.csv" \
-  --nodes "/csvs/headers/version.csv,/csvs/version/$TAG.csv" \
-  --relationships "/csvs/headers/appears_in.csv,/csvs/appears_in/$TAG.csv" \
-  --relationships "/csvs/headers/describes.csv,/csvs/describes/$TAG.csv" \
+  --nodes "/csvs/headers/resource.csv,$RESOURCE_FILES" \
+  --relationships "/csvs/headers/appears_in.csv,$APPEARS_IN_FILES" \
   --id-type=INTEGER \
   --multiline-fields=true
 
