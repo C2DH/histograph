1c5e3e8bd85e8a2f3b98c06ccb6eb290d32f6800
theorm <theorm@gmail.com>
2019-12-02 09:42:42 +0100

    search box help text added

--- client/src/js/controllers/merge-entities.js
+++ client/src/js/controllers/merge-entities.js
@@ -8,15 +8,33 @@ const styles = {
   },
   searchContainer: {
     display: 'flex',
-    flexDirection: 'row',
+    flexDirection: 'column',
     justifyContent: 'center',
+    alignItems: 'center',
+    width: '100%',
     padding: [[theme.units(1), 0]],
     borderBottom: '1px solid #d8d8d8',
     boxShadow: '0px 6px 6px #33333333'
   },
+  inputsWrapper: {
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  inputsContainer: {
+    display: 'flex',
+    flexDirection: 'row'
+  },
   searchInput: {
     width: theme.units(30),
     marginRight: theme.units(1),
+  },
+  searchBoxHelpText: {
+    margin: 0,
+    padding: 0,
+    fontSize: '10px',
+    lineHeight: '10px',
+    paddingTop: '4px',
+    color: theme.colours.text.light.secondary,
   },
   entitiesContainer: {
     display: 'flex',
