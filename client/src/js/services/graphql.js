f70d33ce361c6151c835c4114e9ee709951b7be5
theorm <theorm@gmail.com>
2019-12-29 14:27:10 -0500

    Fixed JWT token auth for GraphQL

--- client/src/js/services/graphql.js
+++ client/src/js/services/graphql.js
@@ -1,10 +1,24 @@
 import ApolloClient from 'apollo-client'
 import { createHttpLink } from 'apollo-link-http'
 import { InMemoryCache } from 'apollo-cache-inmemory'
+import { ApolloLink, concat } from 'apollo-link'
 
-function graphqlClientFactory(HgSettings) {
+function graphqlClientFactory(HgSettings, AuthService) {
+
+  const httpLink = createHttpLink({ uri: `${HgSettings.apiBaseUrl}/api/graphql` })
+  const authMiddleware = new ApolloLink((operation, forward) => {
+    operation.setContext({
+      headers: {
+        authorization: AuthService.getAccessToken()
+          ? `Bearer ${AuthService.getAccessToken()}`
+          : null,
+      }
+    })
+    return forward(operation)
+  })
+
   return new ApolloClient({
-    link: createHttpLink({ uri: `${HgSettings.apiBaseUrl}/api/graphql` }),
+    link: concat(authMiddleware, httpLink),
     cache: new InMemoryCache(),
     defaultOptions: {
       query: {
